14ecd2c67c0bf730e395718191dab02b10c0f5f4
leonardorodriguesbarbosamaia <114547158+leonardorodriguesbarbosamaia@users.noreply.github.com>
2023-05-29 16:16:58 -0300

    logomenu

--- SlnSistemaHospitalar/Menu_login/Formlogin.Designer.cs
+++ SlnSistemaHospitalar/Menu_login/Formlogin.Designer.cs
@@ -86,6 +86,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(26, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(127, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
