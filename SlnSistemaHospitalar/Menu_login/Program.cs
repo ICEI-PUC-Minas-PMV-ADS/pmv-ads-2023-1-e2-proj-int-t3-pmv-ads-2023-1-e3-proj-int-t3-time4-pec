002c21e3bfedbb57a60fd5563ae656e686876439
leonardorodriguesbarbosamaia <114547158+leonardorodriguesbarbosamaia@users.noreply.github.com>
2023-05-29 17:49:15 -0300

    Atualizandonomenologin

--- SlnSistemaHospitalar/Menu_login/Program.cs
+++ SlnSistemaHospitalar/Menu_login/Program.cs
@@ -16,7 +16,7 @@ namespace Menu_login
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Formlogin());
+            Application.Run(new ProntClinic());
         }
     }
 }
---
+++ SlnSistemaHospitalar/Menu_login/ProntClinic.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Menu_login
+{
+    partial class ProntClinic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProntClinic));
+            this.lbluser = new System.Windows.Forms.Label();
+            this.lblacess = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Location = new System.Drawing.Point(88, 78);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(56, 13);
+            this.lbluser.TabIndex = 0;
+            this.lbluser.Text = "USUARIO";
+            // 
+            // lblacess
+            // 
+            this.lblacess.AutoSize = true;
+            this.lblacess.Location = new System.Drawing.Point(88, 126);
+            this.lblacess.Name = "lblacess";
+            this.lblacess.Size = new System.Drawing.Size(44, 13);
+            this.lblacess.TabIndex = 1;
+            this.lblacess.Text = "SENHA";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(167, 71);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(124, 20);
+            this.txtuser.TabIndex = 2;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(167, 119);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(124, 20);
+            this.txtsenha.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ACESSO";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ProntClinic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 258);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.lblacess);
+            this.Controls.Add(this.lbluser);
+            this.Name = "ProntClinic";
+            this.Text = "ProntClinic";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.Label lblacess;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
